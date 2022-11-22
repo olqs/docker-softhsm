a5991fa61c8c4d6c3fb59c6bbfcc13ad47e9e0e2
Alexander Worlitschek <github@olqs.de>
2022-11-22 12:58:50 +0100

    customized probes

--- init.sh
+++ init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+pin=${SOFTHSM_PIN:1234}
+sopin=${SOFTHSM_SOPIN:0000}
+keyid=${SOFTHSM_KEYID:BEEF}
+PKCS11_DAEMON_SOCKET="tcp://0.0.0.0:${SOFTHSM_PORT:5657}"
+
+
+
 if ! [[ -f /var/lib/softhsm/key.pem ]]
 then
   openssl genrsa -out /var/lib/softhsm/key.pem 4096
@@ -7,9 +14,9 @@ fi
 
 if ! [[ -f /var/lib/softhsm/slot0.db ]]
 then
-  softhsm2-util --init-token --slot 0 --label key --pin 1234 --so-pin 0000
+  softhsm2-util --init-token --slot 0 --label key --pin $pin --so-pin $sopin
   SLOTNO=`softhsm2-util --show-slots | grep ^Slot | sed '1!d;s/Slot \(.*\)/\1/'`
-  softhsm2-util --import /var/lib/softhsm/key.pem --slot $SLOTNO --label key --id BEEF --pin 1234
+  softhsm2-util --import /var/lib/softhsm/key.pem --slot $SLOTNO --label key --id BEEF --pin $pin
 fi
 
 
